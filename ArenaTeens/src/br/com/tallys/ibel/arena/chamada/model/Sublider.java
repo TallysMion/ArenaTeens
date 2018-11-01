21d9c9da47a1c8842ab9c3f9b2ad09fd1fe0dbe5
TallysMion <tallysoliveiramion@gmail.com>
2018-11-01 10:05:51 -0300

    Criação dos usuarios

---
+++ ArenaTeens/src/br/com/tallys/ibel/arena/chamada/model/Sublider.java
@@ -0,0 +1,21 @@
+package br.com.tallys.ibel.arena.chamada.model;
+
+public class Sublider extends User {
+
+	private GA grupo;
+	
+	//Desenvolver Métodos
+	
+	@Override
+	public String mainPage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String toHTML() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ ArenaTeens/src/br/com/tallys/ibel/arena/chamada/model/Teens.java
@@ -0,0 +1,24 @@
+package br.com.tallys.ibel.arena.chamada.model;
+
+import java.util.LinkedList;
+
+public class Teens extends User {
+
+	private Ga grupo;
+	private LinkedList<Relatorio> relatorios;
+	
+	//Desenvolver Métodos
+	
+	@Override
+	public String mainPage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String toHTML() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
