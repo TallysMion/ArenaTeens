76bc9663bae01a8ccd473a5a850fa6f3a8c82abc
Tallys Oliveira Mion <tallysoliveiramion@gmail.com>
2018-12-11 21:42:38 -0200

    Pagina de User nao encontrado

--- ArenaTeens/src/br/com/tallys/ibel/arena/chamada/control/Login.java
+++ ArenaTeens/src/br/com/tallys/ibel/arena/chamada/control/Login.java
@@ -36,8 +36,29 @@ public class Login extends HttpServlet{
 				throw new Exception();
 			}
 			page = usuario.toHTML();
+			request.setAttribute("user", usuario);
 		} catch (Exception e) {
-			page = "usuario não encontrado - " + e.toString();//pagina Login -> usuario não encontrado
+			page =    "<!DOCTYPE html>"
+					+ "<html lang=\"pt-br\">"
+					+ "<head>"
+					+ "<link rel=\"stylesheet\" href=\"css/bootstrap.min.css\">"
+					+ "<meta charset=\"utf-8\">"
+					+ "<meta name=\"viewport\" content=\"width-device-width, initial-scale = 1, shrink-to-fit=no\">"
+					+ "</head>"
+					+ "<body class=\"p-3 mb-2 bg-dark text-white\">" 
+					+ "		<div class=\"container\">" 
+					+ "			<div class=\"row\">" 
+					+ "				<div class=\"col-xs-12 col-sm-10 col-md-8 col-lg-6 col-xl-4 mx-auto\">"
+					+ "					<h1 class=\"text-center\">usuario não encontrado - " + e.toString() + "</h1>"//pagina Login -> usuario não encontrado"
+					+ "					<div class=\"col\"><a class=\"btn btn-primary\" style=\"width: 100%\" href=\"index.html\" role=\"button\">Login</a></div>"
+					+ "             </div>"
+					+ "         </div>"
+					+ "     </div>"
+					+ "		<script src=\"js/jquery-3.2.1.min.js\"></script>"
+					+ "		<script src=\"js/popper.min.js\"></script>"
+					+ "		<script src=\"js/bootstrap.min.js\"></script>"
+					+ "</body>"
+					+ "</html>";
 		}
 		
 		//monta a pagina do usuario
